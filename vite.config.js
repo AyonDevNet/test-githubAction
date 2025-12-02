e1b1f8e0ce7a130ca9a79fb4252c85b54b672001
AyonDevNet <rmcoder7@gmail.com>
2025-12-02 12:26:17 +0600

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/devops-portfolio/',
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].[hash].js`,
-        chunkFileNames: `assets/[name].[hash].js`,
-        assetFileNames: `assets/[name].[hash].[ext]`
-      }
-    }
-  }
+  base: '/test-githubAction/'
 })
